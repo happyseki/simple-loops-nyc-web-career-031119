7b149bee6afd0bea4487b2b3d0cf406b2453bbfa
Elaina <elainaseki@gmail.com>
2019-02-09 23:42:32 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -42,7 +42,7 @@ def until_iterator(number_of_times)
   until i == number_of_times
   puts"Welcome to Flatiron School's Web Development Course!"
   i += 1
-
+  end
 end
 
 def for_iterator(number_of_times)
